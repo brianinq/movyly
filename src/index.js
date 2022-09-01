736862180c966d3d02c8b79c1b3e87e7a964e009
brian <kipkoechbrian0@gmail.com>
2022-09-01 17:10:00 +0300

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("linked");
+const API_KEY = "09a0341a69c2c30c14120fe3b8831a27";
+const SEARCH_ENDPOINT = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`;
+const API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=`
+// const API = "https://api.themoviedb.org/3/movie/550?api_key=09a0341a69c2c30c14120fe3b8831a27"
+const CATEGORIES_ENDPOINT = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+const categories = document.querySelector(".categories")
+const hero = document.querySelector(".hero")
+const main = document.querySelector("main")
+const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280"
+const container = document.querySelector("#container")
+let page = 1
